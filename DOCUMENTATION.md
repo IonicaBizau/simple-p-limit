f79e6523686c096d78d2c09e80d25c83a4ec34cd
Ionică Bizău <bizauionica@gmail.com>
2025-03-30 10:26:35 +0300

    :arrow_up: 1.0.0 :tada:

---
+++ DOCUMENTATION.md
@@ -0,0 +1,14 @@
+## Documentation
+
+You can see below the API reference of this module.
+
+### `simplePLimit(concurrency)`
+A simple implementation of concurent promises with a limit.
+
+#### Params
+
+- **Number** `concurrency`: How many functions returning a promise should run in parallel.
+
+#### Return
+- **Function** The `limit` function.
+
--- example/index.js
+++ example/index.js
@@ -2,4 +2,21 @@
 
 const simplePLimit = require("../lib");
 
-console.log(simplePLimit());
+(async () => {
+  const limit = simplePLimit(2)
+
+  // Time   ------------------------>
+  // Task 1  |--- 1000ms ---|
+  // Task 2  |-500ms|
+  // Task 3         |-300ms|
+  // Task 4                |--700ms-|
+  const sleep = (ms, value) => new Promise(resolve => setTimeout(() => resolve(value), ms));
+  const tasks = [
+    limit(() => { return sleep(1000, 'Task 1'); }),
+    limit(() => { return sleep(500, 'Task 2'); }),
+    limit(() => { return sleep(300, 'Task 3'); }),
+    limit(() => { return sleep(700, 'Task 4'); }),
+  ];
+
+  console.log(await Promise.all(tasks)); // Output: ['Task 1', 'Task 2', 'Task 3', 'Task 4']
+})();
